90bdaf1f915d3c3d0b17012dbb097925985ce362
Emiliano Rodriguez <31290912+Emiliano-Rodriguez@users.noreply.github.com>
2023-11-12 16:40:55 -0600

    Add files via upload

--- pages/weight.py
+++ pages/weight.py
@@ -6,12 +6,13 @@ import plotly.graph_objs as go
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
+from dash.dependencies import Input, Output
 
-
+load_dotenv()
 mongodb_connection_string = os.getenv("MONGODB_CONNECTION_STRING")
 
 # Define the layout for the weights page
-def create_weights_page():
+def create_weights_page(app):
     # Read the CSV data
     df = pd.read_csv("data/weights.csv")
     mongo_client = MongoClient(mongodb_connection_string)
@@ -44,14 +45,69 @@ def create_weights_page():
                     yaxis={"title": "Weight"},
                 ),
             },
-            style={"width": "100%", "position": "absolute", "left": "-1%"},
+            style={"width": "80%", "position": "absolute", "left": "10%"},
         ),
         dcc.Interval(
             id="graph-interval",
-            interval=5 * 1000,  # Refresh every 60 seconds (adjust as needed)
+            interval=10 * 1000,  # Refresh every 60 seconds (adjust as needed)
+            n_intervals=0,
+        ),
+        html.Br(),
+        dcc.Graph(
+            id="weights-chart2",
+            config={"displayModeBar": False},
+            figure={
+                "data": [
+                    go.Scatter(
+                        x=df["date"],
+                        y=df["weight"],
+                        mode="lines",
+                        marker={"color": "blue"},
+                    )
+                ],
+                "layout": go.Layout(
+                    title="Weight Data",
+                    xaxis={"title": "Date"},
+                    yaxis={"title": "Weight"},
+                ),
+            },
+            style={"width": "80%", "position": "absolute", "left": "10%","top": "50%"},
+        ),
+        dcc.Interval(
+            id="graph-interval",
+            interval=10 * 1000,  # Refresh every 60 seconds (adjust as needed)
             n_intervals=0,
         ),
 
     ])
+    @app.callback(
+        Output("weights-chart", "figure"),Output("weights-chart2", "figure"),
+        [Input("graph-interval", "n_intervals")]
+    )
+    def update_graph(n):
+        # Fetch data from MongoDB
+        cursor = collection.find()
+        mongo_data = list(cursor)
+        df = pd.DataFrame(mongo_data)
+
+        # Create a new figure with updated data
+        figure = {
+            "data": [
+                go.Scatter(
+                    x=df["date"],
+                    y=df["weight"],
+                    mode="lines",
+                    marker={"color": "blue"},
+                )
+            ],
+            "layout": go.Layout(
+                title="Weight Data",
+                xaxis={"title": "Date"},
+                yaxis={"title": "Weight"},
+            ),
+        }
+
+        return figure, figure
+
 
     return weights_layout
